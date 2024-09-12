5543ccb95a09a6eb67e1924c8a5a9d06e4d3cdc2
Gerardo Gutierrez Velasco <g.gutierrez@lainter.edu.mx>
2024-09-12 17:20:48 -0600

    Se corrige el flujo del administrate

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,5 +2,5 @@ class Appointment < ApplicationRecord
   belongs_to :patient, class_name: 'Patient', foreign_key: 'patient_id'
   belongs_to :doctor, class_name: 'Doctor', foreign_key: 'doctor_id'
 
-  validates :date, presence: true
+  validates :appointment_date, presence: true
 end
