17852a0b227e9cd085f7443fa61f1010a02e5d4b
Manuel Pino <mpinolivon@gmail.com>
2024-09-12 14:17:59 -0500

    Solo un commit para hacer luego un merge

--- db/schema.rb
+++ db/schema.rb
@@ -116,6 +116,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_11_203624) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "type"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
