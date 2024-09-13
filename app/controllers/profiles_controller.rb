f96507637873c15d26d2b201e113289fe3d8dec4
FERNANDO JOSE ALCANTARA CONDOR <alcantaracjose@gmail.com>
2024-09-13 15:49:12 -0500

    Dashbord client

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,6 @@
 class ProfilesController < ApplicationController
   def home
-    @patient = User.find_by(type: 'Patient') # Suponiendo que el usuario actual es un paciente
+    @patient = User.find_by(type: 'Patient')
     @appointments = Appointment.where(patient_id: @patient.id)
     @recent_studies = MedicalResume.includes(:resume).where(user_id: @patient.id)
   end
